36d137dc361d8e364c4dd3789a94414124798604
Wikigoo <135874587+wikigoo@users.noreply.github.com>
2026-07-13 20:26:09 +0330

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'HMR',
+			title: 'My Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
