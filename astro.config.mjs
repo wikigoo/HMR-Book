38705362610f59787491013f7e32587478f55aa0
Wikigoo <135874587+wikigoo@users.noreply.github.com>
2026-07-13 16:44:13 +0330

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import starlightAutoSidebar from 'starlight-auto-sidebar';
-import starlightSiteGraph from 'starlight-site-graph';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://book.hmrbot.com',
 	integrations: [
 		starlight({
-			title: 'HMR Book',
-			plugins: [
-				starlightAutoSidebar(),
-				starlightSiteGraph(),
-			],
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/wikigoo/HMR-Book' }],
+			title: 'HMR',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
-				{ label: 'Astro', collapsed: true, items: [{ autogenerate: { directory: 'astro' } }] },
-				{ label: 'Checklist', collapsed: true, items: [{ autogenerate: { directory: 'Checklist' } }] },
-				{ label: 'Cloudflared', collapsed: true, items: [{ autogenerate: { directory: 'cloudflared' } }] },
-				{ label: 'FlowiseAI', collapsed: true, items: [{ autogenerate: { directory: 'flowiseai' } }] },
-				{ label: 'Flutter', collapsed: true, items: [{ autogenerate: { directory: 'flutter' } }] },
-				{ label: 'Marketing', collapsed: true, items: [{ autogenerate: { directory: 'Marketing' } }] },
-				{ label: 'Other', collapsed: true, items: [{ autogenerate: { directory: 'Other' } }] },
-				{ label: 'Script', collapsed: true, items: [{ autogenerate: { directory: 'Script' } }] },
-				{ label: 'UI', collapsed: true, items: [{ autogenerate: { directory: 'UI' } }] },
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					items: [{ autogenerate: { directory: 'reference' } }],
+				},
 			],
-			customCss: ['./src/styles/custom.css'],
 		}),
 	],
 });
